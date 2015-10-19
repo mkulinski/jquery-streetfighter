08057e97f9dc611462cf8bbd16fd5d5aaa434c26
Michael <me@michaelkulinski.com>
2015-10-19 12:11:19 -0500

    Finished!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+	$(window).keydown(function (e) {
+			if (e.keyCode == 88) {
+				playHadouken();
+				$(".ryu-ready").hide();
+				$(".ryu-throwing").show();
+				$('.hadouken').finish().show().animate(
+				  {'left': '1020px'},
+				  500,
+				  function() {
+				    $(this).hide();
+				    $(this).css('left', '520px');
+				  }
+				);
+			}
+		});
+	$(window).keyup(function(e) {
+		if (e.keyCode == 88) {
+			$(".ryu-throwing").hide();
+			$(".ryu-ready").show();
+		}
+	});
 	$(".ryu").mouseenter(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
@@ -6,5 +27,28 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
+		$(".ryu-throwing").hide();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$('.hadouken').finish().show().animate(
+		  {'left': '1020px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '520px');
+		  }
+		);
+	})
+	.mouseup(function() {
+		$(".ryu-throwing").hide();
+		$(".ryu-ready").show();
 	})
 });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
